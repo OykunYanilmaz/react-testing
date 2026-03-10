412ba850af76c2ae976b4db5b99378ae157112fd
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-10 19:20:03 +0300

    Exercise: Testing expandableText

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,9 @@
-// import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
+
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  
+  return <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit odio facilis sint beatae id. Error quaerat, voluptatem velit eius veritatis sed tempora voluptas doloremque totam esse reiciendis cum delectus enim repellat tenetur harum accusamus asperiores optio dignissimos corrupti nam eligendi, perferendis quae repudiandae. Ullam, cumque corporis labore sapiente excepturi porro laudantium iusto sit voluptatem modi, distinctio consequuntur quasi voluptas odio hic cum deserunt temporibus. Porro dignissimos corporis, inventore dicta sit sapiente eum veniam consequuntur quasi saepe eligendi non quidem accusantium blanditiis corrupti soluta nemo aliquam voluptatum architecto dolores iure aliquid ut est fuga. Nihil ducimus unde est necessitatibus pariatur error?" />;
 };
 
 export default PlaygroundPage;
