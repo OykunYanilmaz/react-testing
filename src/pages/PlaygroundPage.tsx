4339338af6f3bd16ab21f33e55edbdf322f5cf6a
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-10 22:36:43 +0300

    Exercise: Testing searchbox function

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,7 @@
-import ExpandableText from "../components/ExpandableText";
-
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
-  
-  return <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit odio facilis sint beatae id. Error quaerat, voluptatem velit eius veritatis sed tempora voluptas doloremque totam esse reiciendis cum delectus enim repellat tenetur harum accusamus asperiores optio dignissimos corrupti nam eligendi, perferendis quae repudiandae. Ullam, cumque corporis labore sapiente excepturi porro laudantium iusto sit voluptatem modi, distinctio consequuntur quasi voluptas odio hic cum deserunt temporibus. Porro dignissimos corporis, inventore dicta sit sapiente eum veniam consequuntur quasi saepe eligendi non quidem accusantium blanditiis corrupti soluta nemo aliquam voluptatum architecto dolores iure aliquid ut est fuga. Nihil ducimus unde est necessitatibus pariatur error?" />;
+  return <SearchBox onChange={(text) => console.log(text)} />
 };
 
 export default PlaygroundPage;
