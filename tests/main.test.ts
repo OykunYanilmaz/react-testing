d98e9adcdc284cd3fb65a2a52528a34b47325739
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-12 09:21:55 +0300

    Testing data fetching

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -3,6 +3,7 @@ import { it, expect, describe } from 'vitest'
 describe('group', () => {
     it('should', async () => {
         const response = await fetch('/categories');
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const data = await response.json();
         console.log(data);
 
